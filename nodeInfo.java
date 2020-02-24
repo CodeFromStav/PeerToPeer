e750e3d2da6f09eedca9798d2decc97b64206574
Stavros <stav.triantis@gmail.com>
2020-02-24 15:42:23 -0700

    created object array in nodeInfo class

---
+++ nodeInfo.java
@@ -0,0 +1,13 @@
+class ObjectArray
+{
+    //Need to be able to append a new Node Object to Object Array
+    public static void main (String args[])
+    {
+        int x = 3;
+        //Stores node location (x), something
+        Node nodeInfo[] = new Node[x][y];
+        nodeInfo[0] = new Node(); //add multidimensional
+        nodeInfo[1] = new Node();// these are just testing, we need to push nodes into array when joining
+        nodeInfo[3] = new Node();
+    }
+}
