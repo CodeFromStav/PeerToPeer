16e57ef4f6cd248ecfeb2c461194335a620ac29f
Stavros <stav.triantis@gmail.com>
2020-02-25 18:19:55 -0700

    added messageCode cases using switch in setContent

--- Source/message/Message.java
+++ Source/message/Message.java
@@ -12,17 +12,58 @@ public class Message extends MessageTypes
 
     public int getType()
     {
+        if( )
 
     }
     //shouldn't this be type string, not void?
-    public String setContent( String message)
+    public String setContent( String messageCode)
     {
+        String outputStr;
+        switch( messageCode )
+        {
+            case 1: messageCode = 100;
+            outputStr = nodeName + " would like to join.";
+            break;
+
+            case 2: messageCode = 101;
+            outputStr = nodeName + " has joined the chat.";
+            break;
+
+            case 3: messageCode = 105;
+            outputStr = nodeName + " has left the chat.";
+            break;
+            
+            //this one pry isn't right. We must insert the message they input
+            case 4: messageCode = 110;
+            outputStr = nodeName + " message: " + message( ChatNode );
+            break;
+
+
+        }
+
+
+        if ( message == 100 )
+        {
+            //broadcast join req message
+        }
+        else if( message == 101 )
+        {
+            //broadcast message joined
+        }
+        else if( message = 105 )
+        {
+            //broadcast leave message
+        }
+        else if( message = 110 )
+        {
+
+        }
 
     }
 
     public String getContent()
     {
-        
+
     }
 
 
--- Source/message/MessageTypes.java
+++ Source/message/MessageTypes.java
@@ -1,3 +1,12 @@
 package message;
 
 //Establish type of message, so we can check message codes in Message.java
+
+public class MessageTypes
+{
+    //Initialize codes with arbritrary values
+    int JOIN_CODE = 100;
+    int JOINED_CODE = 101;
+    int LEAVE_CODE = 105;
+    int NOTE_CODE = 110;
+}
