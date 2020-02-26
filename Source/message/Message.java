5d39e5a1fdcf286dc41cbe443f253595ee6ad5b3
Stavros <stav.triantis@gmail.com>
2020-02-25 18:05:32 -0700

    populated Message class with functions

--- Source/message/Message.java
+++ Source/message/Message.java
@@ -3,3 +3,27 @@ package message;
 //Contain informaton for what type of message gets set over network
 //Each code corresponds to message
 
+public class Message extends MessageTypes
+{
+    public static void message( int ChatNode )
+    {
+
+    }
+
+    public int getType()
+    {
+
+    }
+    //shouldn't this be type string, not void?
+    public String setContent( String message)
+    {
+
+    }
+
+    public String getContent()
+    {
+        
+    }
+
+
+}
