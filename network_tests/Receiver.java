d78fef7df51ae348a9b8c47e11f07e9b6975bdee
Matt <mba75@nau.edu>
2020-02-27 19:13:35 -0700

    NodeInfo and Receiver files to be used with the tests

---
+++ network_tests/Receiver.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+
+/* Class to sepearate instances of server and client connections */
+public class Receiver implements Runnable {
+    // running thread associated with client
+    Thread currentThread;
+    // socket associated with client's connection
+    IPAddress nodeIP;
+
+    Receiver(IPAddress ip) {
+        nodeIP = ip;
+    }
+    /* Runnable portion of the class, opens up a connection between the client and
+    server */
+    public void run() {
+        while (true)
+        {
+            InetAddress nodeAddress = InetAddress.getByName(nodeIP.toString());
+            ServerSocket server = new ServerSocket(8080,50,nodeAddress);
+            while (true)
+            {
+                Socket serverClient = server.accept();
+                System.out.println("Someone is trying to message");
+                ReceiverHelper inMessage = new ReceiverHelper(serverClient);
+                inMessage.start();
+            }
+        }
+    }
+
+    /* Creates a new thread to start a server-client connection */
+    public void start() {
+        if (currentThread == null) {
+            currentThread = new Thread(this);
+            currentThread.start();
+        }
+    }
+}
